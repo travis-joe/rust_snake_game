eb859625b69ef618e85d0d77cde880faab26048e
qzy <weelgod@gmail.com>
2019-05-23 22:02:20 +0800

    fix：game

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-extern crate rand;
 extern crate piston_window;
+extern crate rand;
 
 mod draw;
+mod game;
 mod snake;
 
+fn multiplication_table() {
+    let mut s = "".to_string();
+    for r in 1..9 + 1 {
+        for c in 1..r + 1 {
+            s += &format!("{}x{}={}   ", c, r, r * c);
+        }
+        s += "\n";
+    }
+
+    print!("{}", s);
+}
+
 fn main() {
-    println!("Hello, world!");
+    multiplication_table();
+
+    //    let mut s = "1".to_string();
+    //    s += "1";
+    //    print!("{}", s);
 }
